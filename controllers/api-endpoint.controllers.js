121b6be1077daf345c11a0c65514e5876cbf9317
OJ423 <osmith.dpm@gmail.com>
2024-01-15 15:41:45 +0000

    Updated code to following comments about tickets 3 and 4

--- controllers/api-endpoint.controllers.js
+++ controllers/api-endpoint.controllers.js
@@ -1,9 +1,9 @@
-const {selectApiEndpoints} = require('../models/api-endpoint-model')
+const {selectApiEndpoints} = require('../models/api-endpoint-models')
 
 exports.getApiEndpoints = (req, res, next) => {
     selectApiEndpoints()
     .then((response) => {
-        res.status(200).send(response)
+        res.status(200).send({endpoints:response})
     })
     .catch((err) => {
         next(err)
